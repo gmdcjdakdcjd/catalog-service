29ab65468ea258bfee7c5d7ccd006954f7f33c55
gmdcjdakdcjd <ysh010223@naver.com>
2024-09-05 10:51:45 +0900

    v11

--- src/main/resources/db/migration/V3__Add_user_audit.sql
+++ src/main/resources/db/migration/V3__Add_user_audit.sql
@@ -1,5 +1,11 @@
-ALTER TABLE book
-    ADD COLUMN created_by varchar(255);
+-- ALTER TABLE book
+--     ADD COLUMN created_by varchar(255);
+--
+-- ALTER TABLE book
+--     ADD COLUMN last_modified_by varchar(255);
 
 ALTER TABLE book
-    ADD COLUMN last_modified_by varchar(255);
+    ADD COLUMN IF NOT EXISTS created_by varchar(255);
+
+ALTER TABLE book
+    ADD COLUMN IF NOT EXISTS last_modified_by varchar(255);
