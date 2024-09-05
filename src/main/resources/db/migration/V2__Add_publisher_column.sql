45cd673c8a910cbb4fe310db3a494fab332688ad
gmdcjdakdcjd <ysh010223@naver.com>
2024-09-05 16:33:04 +0900

    v21

--- src/main/resources/db/migration/V2__Add_publisher_column.sql
+++ src/main/resources/db/migration/V2__Add_publisher_column.sql
@@ -1,5 +1,5 @@
--- ALTER TABLE book
---     ADD COLUMN publisher varchar(255);
-
 ALTER TABLE book
-    ADD COLUMN IF NOT EXISTS publisher varchar(255);
+    ADD COLUMN publisher varchar(255);
+
+-- ALTER TABLE book
+--     ADD COLUMN IF NOT EXISTS publisher varchar(255);
